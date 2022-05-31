48d73771ac84201575199a58ce5ee0b0a784fb59
pwqbot <ding00000804@gmail.com>
2022-05-31 19:23:54 +0800

    update nvim

--- mktar.sh
+++ mktar.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
-git submodule update --remote
+rm -rf nvim
+rsync -r --exclude=.git* --exclude=plugin --exclude=.luarc.json ~/.config/nvim .
 tar zcvf ../offline.tar.gz --exclude='**/.git*' .
