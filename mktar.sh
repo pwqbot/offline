d1adf1d24929d02ef93ec7ad4d88f8bb84fcd702
pwqbot <ding00000804@gmail.com>
2022-06-03 12:02:54 +0800

    update

--- mktar.sh
+++ mktar.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 rm -rf nvim
-rsync -r --exclude=.luarc.json --exclude=nvim/.git  ~/.config/nvim .
+rsync -r --exclude=.luarc.json --exclude=nvim/.git --exclude=nvim/.gitignore --exclude=nvim/plugin  ~/.config/nvim .
 tar zcvf ../offline.tar.gz  .
